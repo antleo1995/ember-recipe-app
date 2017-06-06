079a091edfada244e8ae879007ee9c3e65f2b25b
antleo1995 <antleo1995@gmail.com>
2017-06-06 09:22:33 -0400

    Removes code
    Realizing that in my thrashing I probably don't need recipe-edit route
    as it appears to be working in the nested edit route of recipe. Removes
    code from this file so as to not confuse myself.

--- app/routes/recipe-edit.js
+++ app/routes/recipe-edit.js
@@ -1,16 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model (params) {
-    return this.get('store').findRecord('recipe', params.recipe_id);
-  },
-  actions: {
-    save (recipe) {
-      console.log('testing recipe save on recipe-edit route and recipe is: ', recipe);
-      recipe.save()
-      .then(()=>{
-        this.transitionTo('recipes')
-      })
-    }
-  }
+
 });
