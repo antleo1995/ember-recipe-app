0104d29f0fd9532051d440ef90087fcf3e51c83a
antleo1995 <antleo1995@gmail.com>
2017-06-06 09:28:04 -0400

    Finalizes CRUD
    I now have a working edit feature that fully updates the backend api.
    
    Moving on to new features.

--- app/routes/recipe.js
+++ app/routes/recipe.js
@@ -9,7 +9,13 @@ export default Ember.Route.extend({
   model (params) {
     return this.get('store').findRecord('recipe', params.recipe_id);
   },
-  actions: {
-
+    actions: {
+      save (recipe) {
+        console.log('testing recipe save on recipe-edit route and recipe is: ', recipe);
+        recipe.save()
+        .then(()=>{
+          this.transitionTo('recipes')
+        })
+      }
  }
 });
